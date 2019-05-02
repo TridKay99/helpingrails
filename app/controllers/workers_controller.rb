606f38dd70ca6d8d81ca77389eebd580e0ef4920
Tristan Kay <tridkay90@gmail.com>
2019-05-02 11:06:46 +1000

    added customer_id to controller

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,2 +1,10 @@
 class WorkersController < ApplicationController
+  def index
+    # if params[:job_id]
+    #   @workers = worker.jobs
+    # else
+    #   @workers = Worker.all
+    # end
+    raise
+  end
 end
